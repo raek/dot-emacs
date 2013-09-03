816e295f02401f7e7d32503ba48bcd9cecd8123a
Rasmus Svensson <raek@raek.se>
2013-09-03 20:32:29 +0200

    Default to recursive grep with column numbers

--- custom-file.el
+++ custom-file.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(grep-command "grep -nr ")
  '(inhibit-startup-screen t)
  '(package-archives (quote (("marmalade" . "http://marmalade-repo.org/packages/") ("gnu" . "http://elpa.gnu.org/packages/"))))
  '(tab-always-indent (quote complete)))
