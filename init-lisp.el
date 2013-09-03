5b070e3ec5eed9af57524e05e9019d3d71d27246
Rasmus Svensson <raek@raek.se>
2013-09-03 19:08:56 +0200

    Remove dead code copied from emacs-starter-kit

--- init-lisp.el
+++ init-lisp.el
@@ -29,8 +29,5 @@
     (paredit-mode)))
 
 (dolist (mode '(scheme emacs-lisp lisp clojure clojurescript))
-  (when (> (display-color-cells) 8)
-    (font-lock-add-keywords (intern (concat (symbol-name mode) "-mode"))
-                            '(("(\\|)" . 'esk-paren-face))))
   (add-hook (intern (concat (symbol-name mode) "-mode-hook"))
             'raek-turn-on-paredit))
